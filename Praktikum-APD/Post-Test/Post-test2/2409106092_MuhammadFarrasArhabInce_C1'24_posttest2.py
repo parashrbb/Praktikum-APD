f1d13280d92921f50e25983259704453725c7b6e
parashrbb <kungfupantex@gmail.com>
2024-09-17 21:30:57 +0800

    Update 2409106092_MuhammadFarrasArhabInce_C1'24_posttest2.py
    ubah kata kata

--- Praktikum-APD/Post-Test/Post-test2/2409106092_MuhammadFarrasArhabInce_C1'24_posttest2.py
+++ Praktikum-APD/Post-Test/Post-test2/2409106092_MuhammadFarrasArhabInce_C1'24_posttest2.py
@@ -1,22 +1,22 @@
 nama = "Muhammad Farras Arhab Ince"
 nim  = "2409106092"
-harga_gula = 21000
+hargagula = 21000
 
 Diskon = {
-    "diskon_gulaku" : 0.07,
-    "diskon_manis_kita" : 0.11,
-    "diskon_gunung_madu" : 0.13
+    "diskongulaku" : 0.07,
+    "diskonmaniskita" : 0.11,
+    "diskongunungmadu" : 0.13
 }
 
-diskon_gulaku = harga_gula * Diskon["diskon_gulaku"]
-diskon_manis_kita = harga_gula * Diskon["diskon_manis_kita"]
-diskon_gunung_madu = harga_gula * Diskon["diskon_gunung_madu"]
+diskongulaku = hargagula * Diskon["diskongulaku"]
+diskonmaniskita = hargagula * Diskon["diskonmaniskita"]
+diskongunungmadu = hargagula * Diskon["diskongunungmadu"]
 
-harga_setelah_diskon_gulaku = harga_gula - diskon_gulaku
-harga_setelah_diskon_manis_kita = harga_gula - diskon_manis_kita
-harga_setelah_diskon_gunung_madu = harga_gula - diskon_gunung_madu
+HSDgulaku = hargagula - diskongulaku
+HSDmaniskita = hargagula - diskonmaniskita
+HSDgunungmadu = hargagula - diskongunungmadu
 
 print(f"{nama} dengan NIM {nim} ingin membeli gula seharga Rp {harga_gula}")
-print(f"jika dia membeli Gulaku ia harus membayar {harga_setelah_diskon_gulaku} setelah mendapat diskon 7%")
-print(f"jika dia membeli Manis Kita ia harus membayar {harga_setelah_diskon_manis_kita} setelah mendapat diskon 11%")
-print(f"jika dia membeli Gunung Madu ia harus membayar {harga_setelah_diskon_gunung_madu} setelah mendapat diskon 13%" )
+print(f"jika dia membeli Gulaku ia harus membayar {HSDgulaku} setelah mendapat diskon 7%")
+print(f"jika dia membeli Manis Kita ia harus membayar {HSDmaniskita} setelah mendapat diskon 11%")
+print(f"jika dia membeli Gunung Madu ia harus membayar {HSDgunungmadu} setelah mendapat diskon 13%" )
